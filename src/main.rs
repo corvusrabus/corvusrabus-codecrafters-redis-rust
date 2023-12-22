2170446c2b9179034c2b7362fdf905b02f314dc8
Corvin Paul <corvin.paul@outlook.com>
2023-12-22 14:11:39 +0100

    <async>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
-use std::io::{Read, Write};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use tokio::net::TcpListener;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     //
-    let mut buf = [0;512];
-    for stream in listener.incoming() {
+    let mut buf = [0; 512];
+    loop {
+        let stream = listener.accept().await;
         match stream {
-            Ok(mut stream) => {
+            Ok( (mut stream,_)) => {
                 println!("accepted new connection");
-                while let Ok(_size) = stream.read(&mut buf) {
-                    // let rcved = from_utf8(&buf[..size]).unwrap();
-                    // println!("Received {rcved}");
-                    let msg = "+PONG\r\n".as_bytes();
-                    stream.write_all(msg).unwrap();
-                }
-
+                tokio::spawn(async move {
+                    while let Ok(_size) = stream.read(&mut buf).await {
+                        // let rcved = from_utf8(&buf[..size]).unwrap();
+                        // println!("Received {rcved}");
+                        let msg = "+PONG\r\n".as_bytes();
+                        stream.write_all(msg).await.unwrap();
+                    }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
